c1f9add530fb08763b49069f40bbb1bd0cd73e44
callumok2004 <35779365+callumok2004@users.noreply.github.com>
2020-07-12 00:57:04 +0100

    woop

--- server.js
+++ server.js
@@ -21,13 +21,19 @@ app.get("/pageRouter", (req, res) => {
 app.get("/style.css", (req, res) => {
   res.sendFile(`${__dirname}/Assets/style.css`);
 });
+
 app.get("/script.js", (req, res) => {
   res.sendFile(`${__dirname}/Assets/script.js`);
 });
+
 app.get("/partners.json", (req, res) => {
   res.sendFile(`${__dirname}/Assets/partners.json`);
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.get("*", (req, res) => {
+  res.redirect("/");
 });
