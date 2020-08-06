390be487f1e20a12622f4e4a1e37ebc3781cc408
Callum OKane <35779365+callumok2004@users.noreply.github.com>
2020-08-06 17:27:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("express")(),
   url = require("url"),
   fs = require("fs");
 
+app.use(express.static(__dirname+"/Assets/"))
 app.listen(3001, () => {
   console.log("Your app is listening on port 3001");
 });
@@ -27,22 +28,6 @@ app.get("/warnings/:guildID/:userID", (req, res) => {
     user: req.params.userID
   })
 })
-
-app.get("/style.css", (req, res) => {
-  res.sendFile(`${__dirname}/Assets/style.css`);
-});
-
-app.get("/script.js", (req, res) => {
-  res.sendFile(`${__dirname}/Assets/script.js`);
-});
-
-app.get("/partners.json", (req, res) => {
-  res.sendFile(`${__dirname}/Assets/partners.json`);
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
 
 app.get("*", (req, res) => {
   res.redirect("/");
